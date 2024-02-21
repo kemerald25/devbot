39f63b861e131882b86394d1115e98cb2d42e9f2
Dev Royale <unityekeoba18@gmail.com>
2024-02-21 22:50:42 +0100

    installed and created routes

--- dev-bot/src/App.jsx
+++ dev-bot/src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Home from './pages/Home';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
